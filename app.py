8088ff2f87872a948bb943e278a548b969a55bda
dibashthapa <dibashthapa55@gmail.com>
2020-01-31 22:11:35 +0545

    initial upload

--- app.py
+++ app.py
@@ -1,42 +1,42 @@
 
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
-import webbrowser
+# import webbrowser
 import time
 import os
+
+
 print("Initialized scraper")
 print("Please Enter Your 9anime url \n")
-url=input()
+url = input()
 
 driver = webdriver.Chrome()
 driver.get(url)
-soup=BeautifulSoup(driver.page_source,"html.parser")
-fileWrite= open("output.html","w")
-fileWrite.write(str(soup))
-webbrowser.get("firefox")
-f=open("output.html","r")
-f1=f.readlines()
-x=re.findall("/[\w]+/[\w]+-[\w]+-[\w]+[.0-9a-z]+/[0-9a-z]+",str(f1))
-urls=[]
-length=len(x)//3
 
 
-for link in range(length*2,len(x)):
-    episodeUrl="https://9anime.to"+x[link]
-    driver.get(episodeUrl)
-    tab = driver.find_element_by_xpath('//span[@data-name="35"]')
-    driver.execute_script("arguments[0].click();", tab)
-    episodes = driver.find_elements_by_css_selector('.server[data-name="35"] a')
-    driver.execute_script("arguments[0].click();", episodes[0])
+# no need to click tab again and again
+tab = driver.find_element_by_xpath('//span[@data-name="35"]')
+driver.execute_script("arguments[0].click();", tab)
+
+episodes = driver.find_elements_by_css_selector('.server[data-name="35"] a')
+for link in episodes:
+    # episodeUrl = "https://9anime.to"+x[link]
+    # driver.get(episodeUrl)
+    driver.execute_script("arguments[0].click();", link)
     time.sleep(5)
     iframe = driver.find_element_by_xpath(
         '//iframe[@style="width: 100%; height: 100%;"]')
     driver.switch_to.frame(iframe)
     videoUrl = driver.find_element_by_tag_name('video').get_attribute("src")
-    time.sleep(3)
 
-    driver.get(videoUrl)
-    time.sleep(3)
+    # driver.get(videoUrl)
+
+    # efficient to start download from js as driver doesnt get side tracked
+    driver.execute_script("window.open('"+videoUrl+"')")
+
+    # driver switch back to default from iframe
+    driver.switch_to.default_content()
 
 driver.close()
